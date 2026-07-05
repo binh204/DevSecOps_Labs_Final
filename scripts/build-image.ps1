c2098f992d2dcfbeb7a8a3df5e0b11dd63f532e0
BinhThanhDuong <binhduong204@gmail.com>
2026-07-05 10:52:13 +0700

    Test 2

--- scripts/build-image.ps1
+++ scripts/build-image.ps1
@@ -1,10 +1,27 @@
 Write-Host ""
 Write-Host "========== BUILD IMAGE =========="
 
-docker build -t juice-shop:latest ./juice-shop
+# Nếu chạy trong GitHub Actions thì dùng số lần chạy
+if ($env:GITHUB_RUN_NUMBER) {
+    $Version = "v$($env:GITHUB_RUN_NUMBER)"
+}
+else {
+    # Chạy thủ công
+    $Version = "dev"
+}
+
+Write-Host "Building image version: $Version"
+
+docker build `
+    -t juice-shop:latest `
+    -t juice-shop:$Version `
+    ./juice-shop
 
 if ($LASTEXITCODE -ne 0) {
     throw "Docker build failed!"
 }
 
 Write-Host "Image built successfully."
+Write-Host "Tags:"
+Write-Host "  juice-shop:latest"
+Write-Host "  juice-shop:$Version"
