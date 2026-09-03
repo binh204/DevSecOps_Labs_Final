cb2215dece4a81ffcac04c318a306b262438a75f
BinhThanhDuong <binhduong204@gmail.com>
2026-09-03 12:46:52 +0700

    Test ZAP 2

--- scripts/build-image.ps1
+++ scripts/build-image.ps1
@@ -1,31 +1,3 @@
-# Write-Host ""
-# Write-Host "========== BUILD IMAGE =========="
-# 
-# # Nếu chạy trong GitHub Actions thì dùng số lần chạy
-# if ($env:GITHUB_RUN_NUMBER) {
-#     $Version = "v$($env:GITHUB_RUN_NUMBER)"
-# }
-# else {
-#     # Chạy thủ công
-#     $Version = "dev"
-# }
-# 
-# Write-Host "Building image version: $Version"
-# 
-# docker build `
-#     -t juice-shop:latest `
-#     -t juice-shop:$Version `
-#     ./juice-shop
-# 
-# if ($LASTEXITCODE -ne 0) {
-#     throw "Docker build failed!"
-# }
-# 
-# Write-Host "Image built successfully."
-# Write-Host "Tags:"
-# Write-Host "  juice-shop:latest"
-# Write-Host "  juice-shop:$Version"
-
 #!/bin/bash
 echo ""
 echo "========== BUILD IMAGE =========="
--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,21 +1,3 @@
-# Write-Host ""
-# Write-Host "========== DEPLOY =========="
-# 
-# $container = docker ps -aq -f name=^juice-shop$
-# 
-# if ($container) {
-#     Write-Host "Removing old container..."
-#     docker rm -f juice-shop
-# }
-# 
-# docker compose up -d --build juice-shop
-# 
-# if ($LASTEXITCODE -ne 0) {
-#     throw "Deploy failed!"
-# }
-# 
-# Write-Host "Deploy completed successfully."
-
 #!/bin/bash
 echo ""
 echo "========== DEPLOY TO TARGET SERVER (VM1) =========="
